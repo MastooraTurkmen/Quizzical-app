d692392022553785a971a810be0c67104b2d301b
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-06-18 07:11:51 +0430

    first commit

--- App.js
+++ App.js
@@ -4,12 +4,8 @@ import Quiz from "./components/Quiz"
 import './App.css';
 
 export default function App() {
-  // first we created this state to show the welcome of quiz
-  
     const [quiz, setQuiz] = React.useState(false)
     
-  //this will switch the state on click jii
-
     function startQuiz() { 
         setQuiz(prev => !prev)
     }
@@ -18,17 +14,7 @@ export default function App() {
         <div>
             <img src="./assets/blob-yellow.png" className="blob-yellow" />
             <img src="./assets/blob-blue.png" className="blob-blue" />
-      {
-      
-      /*
-      so now at fist jii the quiz is false so it will show 
-      <Start handleStart={startQuiz} /> 
 
-      so now lets go in the <Start >
-      
-      */
-      
-      }
             {quiz ? <Quiz playAgain={startQuiz} /> : <Start handleStart={startQuiz} />}
         </div>
     )
