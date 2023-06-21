038b4b499785329987f79f16a69a915b345388cd
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-06-21 07:27:07 +0430

    chaning app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import First from "./First";
-import Questions from "./Questions";
-import { nanoid } from "nanoid";
+import React from "react"
+import Start from "./components/Start"
+import Quiz from "./components/Quiz"
+import './App.css';
 
 export default function App() {
-  const [open, setOpen] = React.useState(false);
+    const [quiz, setQuiz] = React.useState(false)
 
-  function toggleOpen() {
-    setOpen((prevOpen) => !prevOpen);
-  }
-
-  return (
-    <div className="container">
-      <div className="content-container">
-        {open ? <Questions /> : <First toggleOpen={toggleOpen} />}
-      </div>
-    </div>
-  );
+    function startQuiz() { 
+        setQuiz(prev => !prev)
+    }
+    
+    return (
+        <div>
+            <img src="./assets/blob-yellow.png" className="blob-yellow" />
+            <img src="./assets/blob-blue.png" className="blob-blue" />
+            {quiz ? <Quiz playAgain={startQuiz} /> : <Start handleStart={startQuiz} />}
+        </div>
+    )
 }
