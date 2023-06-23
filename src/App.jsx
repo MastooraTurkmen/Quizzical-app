afc37214753bd006bb2d4359186d35aa257ec3eb
Mastoora Turkmen <mastooraturkmen@gmail.com>
2023-06-23 07:46:19 +0430

    importing yellowBlob image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Start from "./components/Start"
 import Quiz from "./components/Quiz"
+import YelloBlob from "./assets/blob-yellow.png"
 
 export default function App() {
     const [quiz, setQuiz] = React.useState(false)
@@ -11,7 +12,7 @@ export default function App() {
     
     return (
         <div>
-            <img src="./assets/blob-yellow.png" className="blob-yellow" />
+            <img src={YelloBlob} className="blob-yellow" />
             <img src="./assets/blob-blue.png" className="blob-blue" />
             {quiz ? <Quiz playAgain={startQuiz} /> : <Start handleStart={startQuiz} />}
         </div>
